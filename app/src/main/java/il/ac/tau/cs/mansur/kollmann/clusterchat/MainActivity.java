9f89b2f137850c7bbc7f6371c261b51c589669cf
alonkol <alonkollmann@gmail.com>
2018-03-12 16:24:19 +0200

    Initial commit

---
+++ app/src/main/java/il/ac/tau/cs/mansur/kollmann/clusterchat/MainActivity.java
@@ -0,0 +1,13 @@
+package il.ac.tau.cs.mansur.kollmann.clusterchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
