39de3cb956307d3e982cc3f747054ccae77c3d4d
alonkol <alonkollmann@gmail.com>
2018-04-07 20:07:15 +0300

    Saving conversation history to app storage

--- app/src/main/java/il/ac/tau/cs/mansur/kollmann/clusterchat/ConversationsManager.java
+++ app/src/main/java/il/ac/tau/cs/mansur/kollmann/clusterchat/ConversationsManager.java
@@ -1,39 +1,31 @@
 package il.ac.tau.cs.mansur.kollmann.clusterchat;
 
+import android.text.TextUtils;
 import android.util.Log;
-
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.Set;
 
-public class ConversationsManager {
+class ConversationsManager {
     private final String TAG = "ConversationsManager";
     private HashMap<DeviceContact, ArrayList<String>> mConversations;
     private HashMap<DeviceContact, AddedMessageFlag> mAddedMessagesFlags;
 
-    public ConversationsManager(){
+    ConversationsManager(){
         mConversations = new HashMap<>();
         mAddedMessagesFlags = new HashMap<>();
     }
 
-    public List<String> getMessagesForConversation(DeviceContact deviceContact, int index){
+    List<String> getMessagesForConversation(DeviceContact deviceContact, int index){
         List<String> allMessages =  safeGetMessages(deviceContact);
         return allMessages.subList(index, allMessages.size());
-    }
-
-    public List<String> getAllMessagesForConversation(DeviceContact deviceContact){
-        return safeGetMessages(deviceContact);
     }
 
     private void initDeviceObjects(DeviceContact deviceContact) {
@@ -45,70 +37,70 @@ public class ConversationsManager {
         }
     }
 
-    public List<String> safeGetMessages(DeviceContact deviceContact){
+    private List<String> safeGetMessages(DeviceContact deviceContact){
         initDeviceObjects(deviceContact);
         return mConversations.get(deviceContact);
     }
 
-    public void addMessage(DeviceContact deviceContact, String message) {
+    void addMessage(DeviceContact deviceContact, String message) {
         initDeviceObjects(deviceContact);
         mConversations.get(deviceContact).add(message);
         mAddedMessagesFlags.get(deviceContact).addedMessage();
         Log.i(TAG, "Added message from device " + deviceContact.getDeviceName() + "\nwith content: " + message);
     }
 
-    public void addMessagesFromHistory(File contact){
+    void addMessagesFromHistory(File contact){
         String contactID = contact.getName();
         String line;
 
         try {
-            // FileReader reads text files in the default encoding.
-            FileReader fileReader =
-                    new FileReader(contactID);
-
-            // Always wrap FileReader in BufferedReader.
-            BufferedReader bufferedReader =
-                    new BufferedReader(fileReader);
+            FileReader fileReader = new FileReader(contactID);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             while((line = bufferedReader.readLine()) != null) {
                 addMessage(new DeviceContact(contactID), line);
             }
 
-            // Always close files.
             bufferedReader.close();
+            fileReader.close();
         }
-        catch(FileNotFoundException ex) {
-            Log.e(TAG,
-                    "Unable to open file '" +
-                            contactID + "'");
-        }
-        catch(IOException ex) {
-            Log.e(TAG,
-                    "Error reading file '"
-                            + contactID + "'");
+        catch(Exception e) {
+            Log.e(TAG,"Failed to read history from file.", e);
         }
     }
 
-    public void writeMessagesToFiles(File convDirectory){
-            for (DeviceContact dc: mConversations.keySet()){
-                String deviceAddress = dc.getDeviceId();
+    void writeMessagesToFiles(File convDirectory){
+        for (DeviceContact dc: mConversations.keySet()){
+            List<String> conversation = mConversations.get(dc);
+            String deviceAddress = dc.getDeviceId();
 
+            try {
+                File file = new File(convDirectory, deviceAddress);
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream);
+                String conversation_string = TextUtils.join("\n", conversation);
+                writer.append(conversation_string);
+                writer.close();
+                fileOutputStream.close();
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to save convesration history to file.", e);
             }
+        }
     }
 
-    public void initObserver(DeviceContact deviceContact, Observer o){
+    void initObserver(DeviceContact deviceContact, Observer o){
         initDeviceObjects(deviceContact);
         mAddedMessagesFlags.get(deviceContact).addObserver(o);
         Log.d(TAG, "Observer was init for device " + deviceContact.getDeviceName());
     }
 
-    public void discardObserver(DeviceContact deviceContact){
+    void discardObserver(DeviceContact deviceContact){
         mAddedMessagesFlags.get(deviceContact).deleteObservers();
         Log.d(TAG, "Observer was removed for device " + deviceContact.getDeviceName());
     }
 
     class AddedMessageFlag extends Observable{
-        public void addedMessage(){
+        void addedMessage(){
             setChanged();
             notifyObservers();
         }
